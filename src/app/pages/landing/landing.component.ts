cedee48c02f14ca0614f24b207923344af24f41e
Ramiro Gonzalez <ramiro.gonzalez@dexcom.com>
2023-02-10 17:54:05 -0800

    Added alert to landing component when error or unsuccessful login

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -5,29 +5,47 @@ import { Store } from '@ngrx/store';
 import { AppState } from '../../store/app.reducer';
 import { AuthService } from '../../services/auth/auth.service';
 import { isLoading, stopLoading } from '../../store/actions/ui.actions';
+import Swal from 'sweetalert2';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.scss']
+  styleUrls: ['./landing.component.scss'],
 })
 export class LandingComponent implements OnInit {
-
-  constructor(private fb: FormBuilder, private router: Router, private authService: AuthService, private store: Store<AppState>) { }
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private authService: AuthService,
+    private store: Store<AppState>
+  ) {}
 
   loginForm!: FormGroup;
   hidePassword!: boolean;
+  isLoading!: boolean;
+  subscription!: Subscription;
+
+  get invalidLogin(): boolean {
+    return this.loginForm.invalid || this.isLoading;
+  }
 
   ngOnInit(): void {
     this.hidePassword = true;
     this.loginForm = this.fb.group({
       username: [null, [Validators.required, Validators.email]],
-      password: [null, [Validators.required]]
+      password: [null, [Validators.required]],
     });
 
     if (this.authService.isAuthenticated()) {
       this.router.navigate(['/dashboard']);
     }
+
+    this.subscription = this.store.select('ui').subscribe({
+      next: ({ isLoading }) => {
+        this.isLoading = isLoading;
+      },
+    });
   }
 
   onSubmit(): void {
@@ -41,12 +59,18 @@ export class LandingComponent implements OnInit {
 
     this.authService
       .loginUser(username, password)
-      .then((user) => {
+      .then(() => {
         this.store.dispatch(stopLoading());
         this.router.navigate(['/dashboard']);
       })
       .catch((error) => {
         this.store.dispatch(stopLoading());
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: error.message,
+          confirmButtonColor: '#0db02b',
+        });
         console.error(error);
       });
   }
